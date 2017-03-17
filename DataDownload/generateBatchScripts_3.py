62f1a524c3629cd54b6d785f40b775071add67a7
Jacob R Heldenbrand <jacobrh@h2ologin3.ncsa.illinois.edu>
2017-03-17 08:22:18 -0500

    updated download scripts and examples

--- DataDownload/generateBatchScripts_3.py
+++ DataDownload/generateBatchScripts_3.py
@@ -65,22 +65,12 @@ def createScripts(SRA_ID):
     # Write to the file
     shellFile.write("#!/bin/bash\n\n")
 
-    shellFile.write("mkdir -p /dev/shm/sra_files/sra\n\n")
-    shellFile.write("# Make a symlink from RAM to where the local sra database is stored\n")
-    shellFile.write("ln -sf /projects/sciteam/baib/InputData_DoNotTouch/localDB/refseq /dev/shm/sra_files/refseq\n\n")
-
     shellFile.write("# Download the SRA file\n")
     shellFile.write("~/.aspera/connect/bin/ascp -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh -k 1 -T -l800m anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/" + part + " /dev/shm/sra_files/sra\n\n")
 
-    shellFile.write("#Copy the sra file back to projects (but we will use the copy in RAM for processing\n")
-
-    shellFile.write("cp /dev/shm/sra_files/sra/" + SRA_ID + ".sra /projects/sciteam/baib/InputData_DoNotTouch/SRA_files/sra/" + SRA_ID + ".sra\n\n")
-
     shellFile.write("# fastq-dump was configured to look in /dev/shm/sra_files using vdb-config in SRA toolkit bin folder\n")
-
     shellFile.write("# Convert the SRA into fastq\n")
-    shellFile.write("/projects/sciteam/baib/builds/sra-toolkit/sra-tools-2.8.1-3/bin/fastq-dump -v --gzip --split-files -O /projects/sciteam/baib/InputData_DoNotTouch/fastq_files /dev/shm/sra_files/sra/" + SRA_ID + ".sra\n")
-
+    shellFile.write("/projects/sciteam/baib/builds/sra-toolkit/sra-tools-2.8.1-3/bin/fastq-dump -v --gzip --split-files -O /projects/sciteam/baib/InputData_DoNotTouch/fastq_files /dev/shm/sra_files/sra/" + SRA_ID + ".sra\n\n")
     shellFile.close()
 
 def makeJobListFile():
@@ -109,3 +99,4 @@ for i in SRA_list:
     createScripts(i)
 
 makeJobListFile()
+
